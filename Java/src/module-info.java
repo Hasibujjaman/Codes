113fd6466303872eb1c2299c70680c82ee1f1067
Hasibujjaman <2003001@student.ruet.ac.bd>
2025-07-14 11:57:07 +0600

    Changed structure

--- Java/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author emonchowdhury
- *
- */
-module Java {
-}
--- Java/test2.java
+++
@@ -1,6 +0,0 @@
-public class test2 {
-    public static void main(String [] args){
-        test.main(null);
-        System.out.print("Inside test2\n");
-    }
-}
